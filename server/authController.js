1fa96c8c3b398c31ca438d201ff215413e6160e9
ssnell227 <ssnell227@gmail.com>
2020-06-10 09:26:34 -0600

    login functional

--- server/authController.js
+++ server/authController.js
@@ -12,8 +12,8 @@ module.exports = {
     register: async (req, res) => {
         const db = req.app.get('db')
         const {username, password} = req.body
-        const existingUser = await db.get_user_by_username(username)
-        if (existingUser[0]) {
+        const user = await db.get_user_by_username(username)
+        if (user[0]) {
             return res.status(400).send('That username has been taken')
         }
         const salt = bcrypt.genSaltSync(10)
@@ -26,7 +26,19 @@ module.exports = {
         res.status(201).send(req.session.user)
     },
     login: async (req, res) => {
-        
+        const db = req.app.get('db')
+        const {username, password} = req.body
+        const user = await db.get_user_by_username(username)
+        if (!user[0]) {
+            return res.status(400).send('Username does not exist')
+        }
+        const authenticated = await bcrypt.compareSync(password, user[0].password)
+        if (!authenticated) {
+            return res.status(400).send('Incorrect password')
+        }
+        delete user[0].password
+        req.session.user = user[0]
+        res.status(202).send(req.session.user)
     },
     logout: async (req, res) => {
         
